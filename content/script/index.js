58693ac2af53652a65f05f74f6a40b6e7823971d
changer <dingjinchengyx@163.com>
2016-11-04 21:12:54 +0800

    fix pic

--- content/script/index.js
+++ content/script/index.js
@@ -1,11 +1,15 @@
+
+var bannerIndex = 3;
+var showBanner = function (index) {
+		index = index > 3 ? 0 : index;
+		var l = index == 0 ? '0' : '-' + index + '00%';
+		//console.log(l);
+		$("#banner-tab" + bannerIndex).css("background-color","#eeefff");
+		$("#banner-tab" + index).css("background-color","#00eeff");
+		$(".banner-img").velocity({left:l},{ easing: "ease-in-out" });
+		bannerIndex = index;
+};
+
 $(function() {
-	var bannerIndex = 3;
-	var showBanner = function (index) {
-			index = index > 3 ? 0 : index;
-			var l = index == 0 ? '0' : '-' + index + '00%';
-			console.log(l);
-			$(".banner-img").velocity({left:l},{ easing: "ease-in-out" });
-			bannerIndex = index;
-	};
 	setInterval(function() {showBanner(bannerIndex + 1);},5000);
 });
